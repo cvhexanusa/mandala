71de1fd1b4c394aee61527bafe216d46ea39fcb5
Utep Sutiana <infoutep81@gmail.com>
2026-07-06 11:34:14 +0700

    perbauran

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,13 @@ import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    base: command === "build" ? "/mandala-pegawai/" : "/",
-    plugins: [react(), tailwindcss(), svgr()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        stream: path.resolve(__dirname, "./src/stubs/stream.ts"),
-      },
+export default defineConfig({
+  base: "/",
+  plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      stream: path.resolve(__dirname, "./src/stubs/stream.ts"),
     },
-  };
+  },
 });
